2f366c14c6784adeef3afb040268abf7ecdf04aa
Zhirnov Alexey <alex535_70@mail.ru>
2021-10-16 13:55:11 +0700

    therd commit

--- ClearPluginsCache.cmd
+++ ClearPluginsCache.cmd
@@ -4,11 +4,11 @@ setlocal
 
 call :clear "hkcu\software\far2\pluginscache"
 for /f %%i in ('reg query "hkcu\software\far2\users"') do call :clear %%i\pluginscache
-
+asdads
 goto :eof
 
 :clear
-reg query %1 >nul 2>^&1
+
 if not errorlevel 1 (
 echo.
 echo deleting %1...
